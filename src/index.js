c7b25e38bdf86b5642c21e7206bb1ee8ca1e4ead
Amanda <main@Amandas-MacBook-Air.local>
2020-06-30 02:34:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBreeds();
+  loadBreedOptions();
+});
+
+//fetches breed images
+function fetchBreeds() {
+  const imgURL = 'https://dog.ceo/api/breeds/image/random/4'
+  fetch(imgURL)
+    .then(resp => resp.json()) //parse response as json
+    .then(results => {results.message.forEach(pic => addPic(pic))
+    });
+}
+
+function addPic(dogPic) {
+  let container = document.querySelector('#dog-image-container');
+  let newElement = document.createElement('img');
+  newElement.src = dogPic;
+  container.appendChild(newElement);
+}
+
+function loadBreedOptions() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
+
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+}
