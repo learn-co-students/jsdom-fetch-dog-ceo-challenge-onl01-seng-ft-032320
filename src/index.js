1944bafc1faae189b44d8c9729b7f14556da0af1
nisticism <nistacular@gmail.com>
2020-07-19 00:43:07 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchDogs() {
+    return fetch(imgUrl)
+      .then(resp => resp.json())
+      .then(json => addImages(json))
+  }
+
+  function addImages(dogs) {
+    const main = document.querySelector('div');
+    console.log(`${dogs=>message}`);
+    for(let i = 0; i < dogs.length; i++) {
+        const dog = document.createElement('IMG');
+        dog.src = dogs[i];
+        console.log(`${dogs[i]}`);
+        main.appendChild(dog);
+    }
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    fetchDogs();
+  });
+
+
